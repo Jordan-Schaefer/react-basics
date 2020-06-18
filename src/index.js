c9448b5bfff6d526f3e64209274653452078d028
Jordan Schaefer <jschaef99@gmail.com>
2020-06-18 11:35:37 -0700

    db.json file added and mapped through

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import JSON from './db.json'
+
+// components
+
+import Header from './components/header';
+import NewsList from './components/news-list';
 
 
-const App = () => {
-  return (
-    <div className='hey'>
-      <h1>Hello React !!</h1>
-      <div>Hey</div>
-    </div>
-  )
+class App extends Component {
+
+  state = {
+    news: JSON
+  }
+
+  render () {
+
+    return (
+      <div>
+        <Header />
+        <NewsList news={this.state.news}>
+          Hello
+        </NewsList>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
