ba77f915e7f8162c441ae529a46cc4cc7b545f66
Jordan Schaefer <jschaef99@gmail.com>
2020-06-22 15:09:27 -0700

    filter news in search input

--- src/index.js
+++ src/index.js
@@ -11,15 +11,28 @@ import NewsList from './components/news-list';
 class App extends Component {
 
   state = {
-    news: JSON
+    news: JSON,
+    filtered: []
+  }
+
+  getKeyword = (event) => {
+    // console.log(event.target.value);
+    let keyword = event.target.value;
+    let filtered = this.state.news.filter((item) => {
+      return item.title.indexOf(keyword) > -1
+    });
+    this.setState({
+      filtered
+    })
+    // console.log(filter)
   }
 
   render () {
-
+    const filt = this.state.filtered;
     return (
       <div>
-        <Header />
-        <NewsList news={this.state.news}>
+        <Header keyWords={this.getKeyword}/>
+        <NewsList news={filt.length === 0 ? this.state.news : filt}>
           Hello
         </NewsList>
       </div>
